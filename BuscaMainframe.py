0b3104c3be7e4b5d275a3d5e9cd1d0c4acdc759f
Alexandre Perissinotto <68646755+aperissinotto@users.noreply.github.com>
2020-11-17 21:20:35 -0300

    Update BuscaMainframe.py

--- BuscaMainframe.py
+++ BuscaMainframe.py
@@ -3,12 +3,25 @@ import socket
 port = 23
 msg = ''
 erro = 0
+uread = open('primeiro.ip', 'r')
+uwrite = open('ultimo.ip', 'w')
+radd = open('encontrado.ip', 'a')
+
+host = uread.read()
+print ('iniciando por: ' + host)
 
 for ip1 in range(1, 255):
     for ip2 in range(1, 255):
         for ip3 in range(1, 255):
             for ip4 in range(1, 255):
-                ipf = str(ip1) + "." + str(ip2) + "." + str(ip3) + "." + str(ip4)
+                ipf = str(ip1) + '.' + str(ip2) + '.' + str(ip3) + '.' + str(ip4)
+                a1 = (int(''.join(ipf.split('.'))))
+                a2 = (int(''.join(host.split('.'))))
+                if a1 < a2:
+                    continue
+                uwrite = open('ultimo.ip', 'w')
+                uwrite.write(ipf)
+                uwrite.close()
                 dest = (ipf, port)
                 try:
                     tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -20,6 +33,8 @@ for ip1 in range(1, 255):
                 except socket.error:
                     erro = erro + 1
                 if msg == b'\xff\xfd(':
-                    print('find on ' + host)
+                    radd.write('n' + ipf)
+                    print('find on ' + ipf)
 
 print ('total de erros: ' + str(erro))
+
